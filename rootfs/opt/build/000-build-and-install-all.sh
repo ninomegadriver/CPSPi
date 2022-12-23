835fb20132606f0ba9fb712e168775ea387967ef
nino@nino.com.br <nino@nino.com.br>
2022-12-22 22:35:00 -0300

    sync rootfs

--- rootfs/opt/build/000-build-and-install-all.sh
+++ rootfs/opt/build/000-build-and-install-all.sh
@@ -1,32 +1,54 @@
 #!/bin/bash
 
-echo Building Kernel...
+# Lame, but let's keep it as clean as possible...
+export CFLAGS="-w"
+
+echo First, install some needed packages...
+sudo apt install -y git bc bison flex libssl-dev make build-essential autoconf automake
+
+echo Building the modded Linux Kernel... hang on, this will take a good while...
 cd /opt/build
 /opt/build/kernel.sh
 
-echo Building Wiring PI...
+echo Building Wiring PI, needed for Advance Mame/Menu/Mess
 cd /opt/build/WiringPi
 sudo ./build
 
-echo Build bannerd...
-cd /opt/build/bannerd
-make && sudo cp bannerd /usr/bin/bannerd
-
-echo Building Advance Mame
+echo Building Advance Mame/Menu/Mess
 cd /opt/build/advancemame/
+./build.sh
 
-echo If everthing was built without any issues,
+if [ ! -f /boot/config.txt-org ]
+then
+	echo Backing up original config.txt
+	sudo cp -aiv /boot/config.txt /boot/config.txt-org
+fi
+
+if [ ! -f /boot/cmdline.txt-org ]
+then
+	echo Backing up original cmdline.txt
+	sudo cp -aiv /boot/cmdline.txt /boot/cmdline.txt-org
+fi
+
+echo Installing new cmdline.txt and config.txt
+sudo cp /opt/build/cmdline.txt /boot/cmdline.txt
+sudo cp /opt/build/config.txt /boot/config.txt
+
+echo
+echo If everthing was build without any issues,
 echo connect your Raspberry PI to your arcade 
 echo monitor via the VGA666 Interface.
+echo
 echo The PI should show up in the original CPS
 echo resolution of 384x224 and go straight into
 echo Advance Menu.
-echo .
+echo
 echo Use P1 Button 1 to start a game
 echo Use P2 Button 2 to start a clone
-echo P1 UP + P1 Start   => Opens the Options Menu
-echo P1 DOWN + P! Start => Opens the Load/Save/Exit Menu
-echo.
+echo
+echo P1 UP + P1 Start   \=\> Opens the Options Menu
+echo P1 DOWN + P! Start \=\> Opens the Load/Save/Exit Menu
+echo
 echo Have fun!
 echo \[\]\'s
 echo Nino MegaDriver
